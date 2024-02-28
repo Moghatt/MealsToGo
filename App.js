0f240d213a875505af5cc670a45b6c9967c8d8e2
Moghatt <mohammad_ghatali_kh@yahoo.com>
2024-02-28 17:43:15 +1030

    add infrasture theme and add expo google font

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants-screen";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
+
 export default function App() {
-  return <RestaurantsScreen />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RestaurantsScreen />
+    </ThemeProvider>
+  );
 }
--- src/features/restaurants/screens/restaurants-screen.js
+++ src/features/restaurants/screens/restaurants-screen.js
@@ -1,39 +1,41 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 import { PaperProvider, Searchbar } from "react-native-paper";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import styled from "styled-components/native";
+
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
 
 function RestaurantsScreen() {
   const [searchQuery, setSearchQuery] = React.useState("");
   return (
     <PaperProvider>
-      <SafeAreaView style={styles.container}>
+      <SafeArea>
         <ExpoStatusBar />
-        <View style={styles.search}>
+        <SearchContainer>
           <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
-        </View>
-        <View style={styles.list}>
+        </SearchContainer>
+        <RestaurantListContainer>
           <RestaurantInfoCard />
-        </View>
-      </SafeAreaView>
+        </RestaurantListContainer>
+      </SafeArea>
     </PaperProvider>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
 
 export default RestaurantsScreen;
