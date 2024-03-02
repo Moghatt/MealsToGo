f4d2c4ec14764c944c60d384b5d733bd00cd9dd0
Moghatt <mohammad_ghatali_kh@yahoo.com>
2024-03-02 15:00:27 +1030

    add navigation and tab and some custome components

--- App.js
+++ App.js
@@ -2,11 +2,56 @@ import React from "react";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants-screen";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function SettingsScreen() {
+  return (
+    <View>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MapsScreen() {
+  return (
+    <View>
+      <Text>MAP!</Text>
+    </View>
+  );
+}
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
+
+const getTabBarIcon = (route, focused, color, size) => {
+  let iconName = focused ? TAB_ICON[route.name] : `${TAB_ICON[route.name]}-outline`;
+  return <Ionicons name={iconName} size={size} color={color} />;
+};
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => getTabBarIcon(route, focused, color, size),
+  tabBarActiveTintColor: "tomato",
+  tabBarInactiveTintColor: "gray",
+});
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen name="Maps" component={MapsScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </ThemeProvider>
+    </NavigationContainer>
   );
 }
--- src/features/restaurants/screens/restaurants-screen.js
+++ src/features/restaurants/screens/restaurants-screen.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "react-native";
+import { StatusBar, FlatList } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 import { PaperProvider, Searchbar } from "react-native-paper";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
@@ -6,34 +6,51 @@ import React from "react";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
-`;
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 function RestaurantsScreen() {
   const [searchQuery, setSearchQuery] = React.useState("");
   return (
     <PaperProvider>
-      <SafeArea>
-        <ExpoStatusBar />
-        <SearchContainer>
-          <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
-        </SearchContainer>
-        <RestaurantListContainer>
-          <RestaurantInfoCard />
-        </RestaurantListContainer>
-      </SafeArea>
+      <ExpoStatusBar />
+      <SearchContainer>
+        <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
+      </SearchContainer>
+      <RestaurantList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </PaperProvider>
   );
 }
